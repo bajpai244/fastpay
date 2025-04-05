07106254683117464bde55d9422e1d8ba8f86b9d
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-04-06 00:45:23 +0530

    feat: tx crate

--- crates/tx/src/tx.rs
+++ crates/tx/src/tx.rs
@@ -1,10 +1,11 @@
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
+use sha3::{Digest, Keccak256};
 use state::state::AccountAddress;
 
 pub enum Tx {
     Transfer {
         from: AccountAddress,
-        // TODO: we want to allow transfer to multiple addresses, this later on needs to be an array 
+        // TODO: we want to allow transfer to multiple addresses, this later on needs to be an array
         to: AccountAddress,
         amount: u64,
     },
@@ -20,7 +21,25 @@ impl Tx {
     }
 
     pub fn tx_hash(&self) -> Bytes {
+        let value = self.to_bytes();
 
-        let value = Bytes ::new();
+        let mut hasher = Keccak256::new();
+        hasher.update(value);
+
+        let hash = Bytes::from(hasher.finalize().to_vec());
+
+        hash
+    }
+
+    pub fn to_bytes(&self) -> Bytes {
+        let mut value = BytesMut::new();
+        match self {
+            Self::Transfer { from, to, amount } => {
+                value.extend_from_slice(&from);
+                value.extend_from_slice(&to);
+                value.extend_from_slice(&amount.to_be_bytes());
+                value.freeze()
+            }
+        }
     }
 }
